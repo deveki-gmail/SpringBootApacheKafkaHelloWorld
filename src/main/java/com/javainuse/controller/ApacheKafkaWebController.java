454ebb8f7a96b966d45f7ce1b4554f5e689dc549
deveki nandan mishra <devekinandan@gmail.com>
2022-02-20 14:24:25 +0530

    *updated
    *updated

--- src/main/java/com/javainuse/controller/ApacheKafkaWebController.java
+++ src/main/java/com/javainuse/controller/ApacheKafkaWebController.java
@@ -1,11 +1,13 @@
 package com.javainuse.controller;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.github.javafaker.Faker;
 import com.javainuse.service.KafkaSender;
 
 @RestController
@@ -21,6 +23,29 @@ public class ApacheKafkaWebController {
 
 		return "Message sent to the Kafka Topic java_in_use_topic Successfully";
 	}
+	
+	@GetMapping(value = "/startproducer")
+	public String start() {
+		
+		
+		new Thread(new MyRunnable(kafkaSender)).start();
+
+		return "Producer started successfully";
+	}
 
 }
-
+class MyRunnable implements Runnable{
+	
+	KafkaSender kafkaSender;
+	
+	public MyRunnable(KafkaSender kafkaSender){
+		this.kafkaSender = kafkaSender;
+	}
+	
+	public void run(){
+		Faker faker = new Faker();
+		while(true){
+			kafkaSender.send(faker.hobbit().quote());
+		}
+	}
+}
